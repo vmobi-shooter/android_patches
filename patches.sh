fff114f712bd1b2405848e86e64c1402b0a93088
Joey Troy <joey@joeytroy.com>
2013-04-19 14:58:15 -0600

    Update patches.sh
    Added msm8660 kernel, requested to add it back into the cm-10.1 branch.

--- patches.sh
+++ patches.sh
@@ -8,3 +8,8 @@ echo "Installing patch 34409"
 git pull http://review.cyanogenmod.org/CyanogenMod/android_frameworks_av refs/changes/09/34409/7
 
 cd $TOPDIR
+
+echo "Downloading msm8660 kernel"
+git clone https://github.com/CyanogenMod/android_kernel_htc_msm8660.git -b cm-10.1-ds
+
+cd $TOPDIR
